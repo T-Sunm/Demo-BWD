2211209a16d31ce868aeb337010a204aa8d45c03
T-Sunm <moonlig.73@gmail.com>
2023-05-10 16:11:15 +0700

    lam step 1,2,5,6

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        desktop: { min: '1401px' },
+        laptop: { max: '1400px', min: '1150px' },
+        tablet: { max: '1149px', min: '800px' },
+        mobile: { max: '799px' }
+      },
+      colors: {
+        'colorButtonHeader': "#dddddd"
+      },
+      top: {
+
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
