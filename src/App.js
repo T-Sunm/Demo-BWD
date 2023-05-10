27549c105f9e8ea888b792cf8a02c34e20357d29
T-Sunm <moonlig.73@gmail.com>
2023-05-10 19:15:57 +0700

    lam step 7,8,9

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Step1CHome from './components/Step1CHome'
 import Step2CHome from './components/Step2CHome';
 import Step5CHome from './components/Step5CHome';
 import Step6CHome from './components/Step6CHome';
+import Step7CHome from './components/Step7CHome';
+import Step8CHome from './components/Step8CHome';
+import Step9Home from './components/Step9Home';
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <Step1CHome /> */}
       {/* <Step2CHome /> */}
-      <Step5CHome />
+      {/* <Step5CHome /> */}
       {/* <Step6CHome /> */}
+      {/* <Step7CHome /> */}
+      {/* <Step8CHome /> */}
+      <Step9Home />
     </div>
   );
 }
